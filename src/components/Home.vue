46e24c2cc63401ffb632693f4103c96375f103f1
Kirill Skorikov <skorikovkrpmi@gmail.com>
2023-04-12 00:22:15 +0500

    home component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,29 @@
 <template>
-  <h1>Welcome to WebChat.</h1>
-  <p>Used solution stack:</p>
-  <ul>
-    <li>ASP .NET Core Web API</li>
-    <li>Entity Framework</li>
-    <li>SQL Server</li>
-    <li>Vue</li>
-    <li>SignalR</li>
-    <li>Entity Framework</li>
-  </ul>
-  <ul></ul>
+  <div class="main-container">
+    <h1>Welcome to WebChat.</h1>
+    <p>Used solution stack:</p>
+    <ul class="stack">
+      <li>ASP .NET Core Web API</li>
+      <li class="description">
+        Backend that exposes API for getting chat history, adding friends and making chat rooms.
+        Also provides authentication and authorization with Identity framework.
+      </li>
+      <li>Entity Framework</li>
+      <li class="description">ORM for my relational database.</li>
+      <li>SQL Server</li>
+      <li class="description">
+        Relational database for storing friends relationships, chat histories and info about chat rooms.
+      </li>
+      <li>Vue</li>
+      <li class="description">
+        Frontend framework for SPA that provides routing and component system.
+      </li>
+      <li>SignalR</li>
+      <li class="description">
+        Backend lib and browser client for realtime message sending between clients.
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -19,5 +33,28 @@ export default {
 </script>
 
 <style scoped>
+  .main-container {
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    max-width: 70%;
+  }
 
+  h1 {
+    margin-bottom: 10px;
+  }
+
+  .stack {
+    margin-top: 20px;
+    margin-left: 20px;
+    list-style: none;
+    text-align: start;
+  }
+
+  .description {
+    margin-left: 20px;
+    margin-bottom: 10px;
+    margin-top: 5px;
+    font-style: italic;
+  }
 </style>
